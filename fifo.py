f81dd74b35a1f7a8d2af702c25832a3dc1e7692c
Karen <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2025-10-18 17:02:06 -0300

    Create fifo.py

---
+++ fifo.py
@@ -0,0 +1,71 @@
+memoria = [ ]
+page_faults = 0
+
+paginas = [4, 3, 25, 8, 19, 6, 25, 8, 16, 35, 45, 22, 8, 3, 16, 25, 7]
+quadros = 8
+
+for pagina in paginas:
+    
+    if pagina not in memoria:
+        
+        if len(memoria) < quadros:
+            memoria.append(pagina)
+        else:
+            memoria.pop(0)
+            memoria.append(pagina)
+        
+        page_faults = page_faults + 1
+
+
+if 7 in memoria:
+    print("\nA página 7 ficou na posição", memoria.index(7), "da memória.")
+else:
+    print("\nA página 7 não ficou na memória no final.")
+
+memoria = [ ]
+page_faults = 0
+
+paginas = [4, 5, 7, 9, 46, 45, 14, 4, 64, 7, 65, 2, 1, 6, 8, 45, 14, 11]
+quadros = 8
+
+for pagina in paginas:
+    
+    if pagina not in memoria:
+        
+        if len(memoria) < quadros:
+            memoria.append(pagina)
+        else:
+            memoria.pop(0)
+            memoria.append(pagina)
+        
+        page_faults = page_faults + 1
+
+
+if 11 in memoria:
+    print("S página 11 ficou na posição", memoria.index(11), ".")
+else:
+    print("A página 11 não ficou na memória no final.")
+
+memoria = [ ]
+page_faults = 0
+
+paginas = [4, 6, 7, 8, 1, 6, 10, 15, 16, 4, 2, 1, 4, 6, 12, 15, 16, 11]
+quadros = 8
+
+for pagina in paginas:
+    
+    if pagina not in memoria:
+        
+        if len(memoria) < quadros:
+            memoria.append(pagina)
+        else:
+            memoria.pop(0)
+            memoria.append(pagina)
+        
+        page_faults = page_faults + 1
+
+
+if 11 in memoria:
+    print("A página 11 ficou na posição", memoria.index(11), "da memória.")
+else:
+    print("A página 11 também não ficou na memória no final.")
