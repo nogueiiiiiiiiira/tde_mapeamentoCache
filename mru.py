a426ea1920fb8f84c26c8d6dcf70b2b9b87cb558
Karen <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2025-10-18 18:32:08 -0300

    Create mru.py

---
+++ mru.py
@@ -0,0 +1,105 @@
+memoria = []
+page_faults = 0
+ultimo_acesso = {}
+tempo = 0
+
+paginas = [4, 3, 25, 8, 19, 6, 25, 8, 16, 35, 45, 22, 8, 3, 16, 25, 7]
+quadros = 8
+
+for pagina in paginas:
+    tempo += 1
+    ultimo_acesso[pagina] = tempo
+    
+    if pagina not in memoria:
+        page_faults += 1
+        
+        if len(memoria) < quadros:
+            memoria.append(pagina)
+        else:
+            pagina_mru = memoria[0]
+            for p in memoria:
+                if ultimo_acesso[p] > ultimo_acesso[pagina_mru]:
+                    pagina_mru = p
+            
+            memoria.remove(pagina_mru)
+            memoria.append(pagina)
+
+if 7 in memoria:
+    print("\n1A (MRU) - A página 7 ficou na posição:", memoria.index(7))
+else:
+    print("\n1A (MRU) - A página 7 não ficou na memória")
+
+print("Page Faults:", page_faults)
+print("Memória final:", memoria)
+
+
+# reiniciando para 1B
+memoria = []
+page_faults = 0
+ultimo_acesso = {}
+tempo = 0
+
+paginas = [4, 5, 7, 9, 46, 45, 14, 4, 64, 7, 65, 2, 1, 6, 8, 45, 14, 11]
+quadros = 8
+
+for pagina in paginas:
+    tempo += 1
+    ultimo_acesso[pagina] = tempo
+    
+    if pagina not in memoria:
+        page_faults += 1
+        
+        if len(memoria) < quadros:
+            memoria.append(pagina)
+        else:
+            pagina_mru = memoria[0]
+            for p in memoria:
+                if ultimo_acesso[p] > ultimo_acesso[pagina_mru]:
+                    pagina_mru = p
+            
+            memoria.remove(pagina_mru)
+            memoria.append(pagina)
+
+if 11 in memoria:
+    print("\n1B (MRU) - A página 11 ficou na posição:", memoria.index(11))
+else:
+    print("\n1B (MRU) - A página 11 não ficou na memória")
+    
+print("Page Faults:", page_faults)
+print("Memória final:", memoria)
+
+# reiniciando para 1C
+
+memoria = []
+page_faults = 0
+ultimo_acesso = {}
+tempo = 0
+
+paginas = [4, 6, 7, 8, 1, 6, 10, 15, 16, 4, 2, 1, 4, 6, 12, 15, 16, 11]
+quadros = 8
+
+for pagina in paginas:
+    tempo += 1
+    ultimo_acesso[pagina] = tempo
+    
+    if pagina not in memoria:
+        page_faults += 1
+        
+        if len(memoria) < quadros:
+            memoria.append(pagina)
+        else:
+            pagina_mru = memoria[0]
+            for p in memoria:
+                if ultimo_acesso[p] > ultimo_acesso[pagina_mru]:
+                    pagina_mru = p
+            
+            memoria.remove(pagina_mru)
+            memoria.append(pagina)
+
+if 11 in memoria:
+    print("\n1C (MRU) - A página 11 ficou na posição:", memoria.index(11))
+else:
+    print("\n1C (MRU) - A página 11 não ficou na memória")
+
+print("Page Faults:", page_faults)
+print("Memória final:", memoria)
